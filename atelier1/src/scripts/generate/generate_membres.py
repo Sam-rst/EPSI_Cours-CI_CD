4ea894f519f6312042d5922f36a11025d51fe056
samrst <samuel.ressiot@gmail.com>
2024-10-24 13:36:40 +0200

    Atelier terminé, il ne manque plus que de rédiger le readme

---
+++ atelier1/src/scripts/generate/generate_membres.py
@@ -0,0 +1,36 @@
+import csv
+from jinja2 import Environment, FileSystemLoader
+
+def generer_membres():
+    try:
+        # Configurer Jinja2
+        env = Environment(loader=FileSystemLoader('../templates'))
+        template = env.get_template('membres.html')
+    except Exception as e:
+        print(f"Erreur lors de la configuration de Jinja2 : {e}")
+        return
+
+    membres = []
+    
+    with open('../data/csv/membres-bureau-association.csv', 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip header row
+        
+        for row in csv_reader:
+            membres.append({
+                "prenom": row[0],
+                "nom": row[1],
+                "email": row[2],
+                "statut": row[3],
+            })
+        output = template.render(membres=membres)
+        with open('../../public/membres.html', 'w') as f:
+            f.write(output)
+            
+        print("Génération des membres terminée.")
+
+if __name__ == "__main__":
+    try:
+        generer_membres()
+    except Exception as e:
+        print(f"Une erreur inattendue s'est produite : {e}")
--- atelier1/src/scripts/generate_pages.py
+++ atelier1/src/scripts/generate_pages.py
@@ -1,9 +1,11 @@
 from generate.generate_actus import generer_actualites
 from generate.generate_actus_home import generer_actualites_home
+from generate.generate_membres import generer_membres
 
 if __name__ == "__main__":
     try:
         generer_actualites()
         generer_actualites_home()
+        generer_membres()
     except Exception as e:
         print(f"Une erreur inattendue s'est produite : {e}")
